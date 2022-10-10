6514ac1cee1da0fa4d18046e14ae243e832181eb
Daníel Örn Árnason <danielarnason85@gmail.com>
2022-10-10 15:52:58 +0200

    nyt basemap

--- main.js
+++ main.js
@@ -11,18 +11,20 @@ const createMap = async adresse => {
     let data = await dawaData.json()
     let adressCoordinates = [data[0].y, data[0].x]
 
+    let cartoPositronBaseMap = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        maxZoom: 20
+    });
+    
     let map = L.map('map', {
         center: adressCoordinates,
         zoom: zoomLevel,
         zoomControl: false,
+        layers: [
+            cartoPositronBaseMap
+        ]
     });
     map.attributionControl.setPrefix('<a href="https://leafletjs.com" title="A Javascript library for interactve maps">Leaflet</a>')
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    
 
     let adresseMarker = L.marker(adressCoordinates).addTo(map)
 
