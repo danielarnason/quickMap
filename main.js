ac8d41046d6fccea2312b9fd548d0129c1c44e84
Daníel Örn Árnason <danielarnason85@gmail.com>
2023-10-06 14:03:04 +0200

    Custom marker og popup åben fr start

--- main.js
+++ main.js
@@ -26,9 +26,16 @@ const createMap = async adresse => {
     });
     map.attributionControl.setPrefix('<a href="https://leafletjs.com" title="A Javascript library for interactve maps">Leaflet</a>')
 
-    let adresseMarker = L.marker(adressCoordinates).addTo(map)
+    let mapPin = L.divIcon({
+        className: 'custom-marker',
+        html: '<svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="#5274c5" viewBox="0 0 256 256"><path d="M128,16a88.1,88.1,0,0,0-88,88c0,75.3,80,132.17,83.41,134.55a8,8,0,0,0,9.18,0C136,236.17,216,179.3,216,104A88.1,88.1,0,0,0,128,16Zm0,56a32,32,0,1,1-32,32A32,32,0,0,1,128,72Z"></path></svg>',
+        iconSize: [42, 42],
+        popupAnchor: [0, -21]
+    })
 
-    adresseMarker.bindPopup(`<h3>${navn}</h3><br><p>${adresse}</p>`)
+    let adresseMarker = L.marker(adressCoordinates, {icon: mapPin}).addTo(map)
+
+    adresseMarker.bindPopup(`<h3>${navn}</h3><br><p>${adresse}</p>`).openPopup()
     
 }
 
